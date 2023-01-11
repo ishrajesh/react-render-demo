4385a0b7e37e85d518f113344e577c2fc261f013
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-11 16:23:28 +0530

    Initial Changes

---
+++ src/components/useState/UseState.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function UseState() {
+    const [count, setcount] = useState(0)
+    return (
+    <div>UseState</div>
+  )
+}
+
+export default UseState
